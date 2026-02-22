a01224bb6ff950c6df73811008202407074b1acc
Malin Skrettingland <malinersdal@hotmail.com>
2026-02-22 23:17:16 +0100

    Add Thumbnail component for product display

---
+++ src/app/products/[id]/Thumbnail.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Product } from '@/types/product';
+import Image from 'next/image';
+
+interface ThumbnailProps {
+  product: Product;
+}
+
+export function Thumbnail({ product }: ThumbnailProps) {
+  return (
+    <div className="w-40 rounded-sm bg-[var(--color-muted-brown)] p-2">
+      <div className="-mx-1 -mt-1 mb-2 flex justify-center">
+        <Image
+          src={product.image.url}
+          alt={product.image.alt}
+          width={160}
+          height={160}
+          className="h-40 w-40 rounded-sm object-cover"
+        />
+      </div>
+      <div className="flex justify-between gap-2 text-xs">
+        <h3 className="font-heading text-black">{product.tags[0]}</h3>
+        <p className="font-label text-black">{product.price}$</p>
+      </div>
+      <p className="font-label line-clamp-2 text-xs text-black">{product.title}</p>
+    </div>
+  );
+}
