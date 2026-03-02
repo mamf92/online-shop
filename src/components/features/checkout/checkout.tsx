2e24a6f8b144d61aaf0e16d1e5d99e3515d2ea9d
Malin Skrettingland <malinersdal@hotmail.com>
2026-03-02 20:26:02 +0100

    Apply suggestion from @mamf92
    Co-authored-by: Martin Fischer <115947567+mamf92@users.noreply.github.com>

--- src/components/features/checkout/checkout.tsx
+++ src/components/features/checkout/checkout.tsx
@@ -18,7 +18,7 @@ export function Checkout({ Lines, mode }: CheckoutProps) {
   return (
     <div className="mx-auto flex w-full max-w-3xl flex-col px-0 py-10 sm:px-6">
       <div
-        className={`font-heading bg-secondary mb-4 grid w-full items-center px-2 text-[14px] uppercase ${
+        className={`font-heading text-white rounded-xs bg-secondary mb-4 grid w-full items-center px-2 text-[14px] uppercase ${
           mode === 'cart'
             ? 'grid-cols-[minmax(0,1.8fr)_0.8fr_0.7fr_0.8fr_0.3fr]'
             : 'grid-cols-[minmax(0,1.8fr)_0.8fr_0.7fr_0.8fr]'
