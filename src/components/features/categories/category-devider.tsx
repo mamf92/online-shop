30a6f5c19b21008943e5ae3c9173cb0997a27d54
Malin Skrettingland <malinersdal@hotmail.com>
2026-03-02 22:27:45 +0100

    fix: update import statements and adjust category links for consistency

--- src/components/features/categories/category-devider.tsx
+++ src/components/features/categories/category-devider.tsx
@@ -15,16 +15,18 @@ export default function CategoryDivider() {
         <div className="flex flex-row justify-center gap-8 sm:gap-6 md:gap-4">
           {items.map((item) => (
             <div key={item.id} className="flex flex-shrink-0 flex-col items-center">
-              <Image
-                src={item.image}
-                alt={item.name}
-                width={160}
-                height={160}
-                className="mb-4 h-16 w-16 object-cover sm:h-24 sm:w-24 md:h-40 md:w-40"
-              />
-              <p className="font-heading text-muted-brown text-center text-sm uppercase">
-                {item.name}
-              </p>
+              <a href={`/categories/${item.name.toLowerCase()}`} className="">
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  width={160}
+                  height={160}
+                  className="mb-4 h-16 w-16 object-cover sm:h-24 sm:w-24 md:h-40 md:w-40"
+                />
+                <p className="font-heading text-muted-brown text-center text-sm uppercase">
+                  {item.name}
+                </p>
+              </a>
             </div>
           ))}
         </div>
