64b0b22dc21b2914249efe92ceac52d6e5c8bfec
Malin Skrettingland <malinersdal@hotmail.com>
2026-03-07 22:48:36 +0100

    refactor: replace Link with StyledButton for navigation in Cart and Checkout pages

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { ShoppingCart } from '@/components/features/cart/cart';
 import { StyledButton } from '@/components/ui/buttons';
 
@@ -6,7 +7,14 @@ export default function CheckoutPage() {
     <main className="mx-auto w-full max-w-3xl flex-1 px-6 py-10">
       <h1 className="font-hero text-accent text-center text-[2rem]">Checkout</h1>
       <ShoppingCart mode="checkout" />
-      <StyledButton variant={'primary'} className="mx-auto block">
+      {/* Navigate to payment page */}
+      <StyledButton
+        variant={'primary'}
+        onClick={() => {
+          document.location.href = '/cart';
+        }}
+        className="mx-auto block"
+      >
         Pay
       </StyledButton>
     </main>
