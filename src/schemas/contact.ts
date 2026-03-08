7ea534e8833a9e60d90c1b9ea5260225cc37fa24
Martin Fischer <mamfischer92@gmail.com>
2026-03-08 21:31:54 +0100

    fix: add minor change to precisely conform with brief

--- src/schemas/contact.ts
+++ src/schemas/contact.ts
@@ -3,13 +3,13 @@ import { z } from 'zod';
 export const ContactFormSchema = z.object({
   subject: z
     .string()
-    .min(2, { message: 'Subject needs to be at least 2 characters long.' })
+    .min(3, { message: 'Subject needs to be at least 3 characters long.' })
     .max(100, { message: 'Subject cannot be longer than 100 characters.' }),
   name: z
     .string()
-    .min(2, { message: 'Name needs to be at least 2 characters long.' })
+    .min(3, { message: 'Name needs to be at least 3 characters long.' })
     .max(100, { message: 'Name cannot be longer than 100 characters.' }),
-  email: z.string().email({ message: 'Please enter a valid email address.' }),
+  email: z.email({ message: 'Please enter a valid email address.' }),
   message: z
     .string()
     .min(10, { message: 'Message needs to be at least 10 characters long.' })
